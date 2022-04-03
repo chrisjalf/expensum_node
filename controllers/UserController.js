f1782b01ae481bd9048977e3499143b1c8fec1ee
Chris James <>
2022-04-03 21:59:54 +0800

    transaction create api

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const moment = require('moment');
 const { checkSchema, validationResult } = require('express-validator');
 
 const Users = require('../models').users;
@@ -128,6 +129,73 @@ module.exports.transaction = async (req, res) => {
             return ReS(res, '', { transaction: transaction });
 
         throw 'Transaction not found';
+    }).catch(err => {
+        return ReE(res, err, 400);
+    });
+}
+
+module.exports.transactionCreate = async (req, res) => {
+    await checkSchema({
+        cat_id: {
+            notEmpty: true,
+            errorMessage: 'Category ID is required',
+            custom: {
+                options: value => {
+                    return Categories.findOne({
+                        where: { id: value }
+                    }).then(cat => {
+                        if (!cat) return Promise.reject();
+                    });
+                },
+                errorMessage: 'Valid category ID is required'
+            },
+            toInt: true
+        },
+        description: {
+            notEmpty: true,
+            errorMessage: 'Description is required'
+        },
+        type: {
+            notEmpty: true,
+            errorMessage: 'Type is required',
+            isIn: {
+                options: 'Add,Deduct',
+                errorMessage: 'Type must be either Add/Deduct'
+            }
+        },
+        amount: {
+            notEmpty: true,
+            errorMessage: 'Amount is required',
+            isCurrency: {
+                options: { thousands_separator: '' },
+                errorMessage: 'Amount must be a currency'
+            },
+            toFloat: true
+        },
+        trans_date: {
+            notEmpty: true,
+            errorMessage: 'Transaction date is required',
+            custom: {
+                options: value => {
+                    return moment(value).isValid()
+                },
+                errorMessage: 'Valid transaction date is required'
+            }
+        }
+    }).run(req);
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+        return ReE(res, errors.array()[0].msg, 400);
+
+    const body = Keyfilter(req.body, ['cat_id', 'description', 'type', 'amount', 'trans_date']);
+    
+    Transactions.create({
+        user_id: req.user.id,
+        ...body
+    }).then(transaction => {
+        return ReS(res, 'Transaction created', { transaction: transaction });
     }).catch(err => {
         return ReE(res, err, 400);
     });
